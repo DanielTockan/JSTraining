ccaf46ad044d71f3dc1a6c2d48d1753190751671
danielzt94 <danieltockan@gmail.com>
2021-02-18 19:40:00 +0000

    step 10

--- solvingProblems/commonItems.js
+++ solvingProblems/commonItems.js
@@ -67,8 +67,8 @@
 // ! i.e. objects converting O(a*b) into something faster
 // I commented the above out and will now refactor to improve!
 
-const input1 = ['a','b','c','x']
-const input2 = ['z', 'y', '55']
+const input1 = ['a', 'b', 'c', 'x']
+const input2 = ['z', 'y', 'a']
 
 // ! still 7th point
 // ! create function that converts one of the input arrays to an object
@@ -88,12 +88,31 @@ const input2 = ['z', 'y', '55']
 // !
 // ! Do some pseudocode for my final solution before producing final solution
 
-function commonLetter2(arr1,arr2) {
+function commonLetter2(arr1, arr2) {
+
   // loop through first array and create object
   // where properties === item in the array
 
+  let map = {}
+  for (let i = 0; i < arr1.length; i++) {
+    if (!map[arr1[i]]) {
+      const item = arr1[i]
+      map[item] = true
+    }
+  }
+
+  // console.log(map)
+
   // loop through second array and check if item in second array
-  // exists on created object  
+  // exists on created object
+
+  for (let j = 0; j < arr2.length; j++) {
+    if (map[arr2[j]])
+      return true
+  }
+  return false
 }
+
 // O(a+b) rather than O(a*b)
 
+console.log(commonLetter2(input1, input2));
