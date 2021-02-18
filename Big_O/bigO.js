3f78e3965bb64d8396806adb9450bcaf2b1c64a5
danielzt94 <danieltockan@gmail.com>
2021-02-18 01:11:11 +0000

    big O

---
+++ Big_O/bigO.js
@@ -0,0 +1,28 @@
+const { log } = require('console');
+const {performance} = require('perf_hooks')
+
+const nemo = ['nemo'];
+
+const everyone = ['nemo', 'pq', 'p2', 'p3', 'p4', 'p5', 'p6'];
+
+const large = new Array(1000000).fill('nemo')
+
+function findNem0(array) {
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found Nemo!')
+    }
+  }
+}
+
+// findNem0(large) // O (n) --> Linear Time
+
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]);
+  console.log(boxes[1]);
+}
+
+const boxes = [0,9,8,7,6,5]
+
+logFirstTwoBoxes(boxes)
