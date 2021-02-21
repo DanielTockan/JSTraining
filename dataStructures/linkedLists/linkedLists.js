fe88f2dd3fd2338c5076267ef648cb1408a0c6d9
danielzt94 <danieltockan@gmail.com>
2021-02-21 23:33:13 +0000

    linked lists

--- dataStructures/linkedLists/linkedLists.js
+++ dataStructures/linkedLists/linkedLists.js
@@ -1,1 +1,8 @@
-const basket = ['']
+const basket = ['apples', 'grapes', 'pears']
+
+linked list: apples --> grapes --> pears
+
+apples
+8947 --> grapes
+          8742 --> pears
+                    372 --> null
