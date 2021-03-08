34b7066b1d92481b5ea053454e462b6fc76cb834
danielzt94 <danieltockan@gmail.com>
2021-03-08 12:18:49 +0000

    linked lists cont

--- dataStructures/linkedLists/linkedLists.js
+++ dataStructures/linkedLists/linkedLists.js
@@ -1,68 +1,104 @@
-// const basket = ['apples', 'grapes', 'pears']
-
-// linked list: apples --> grapes --> pears
-
-// apples
-// 8947 --> grapes
-//           8742 --> pears
-//                     372 --> null 
-
-
-// 10 --> 5 --> 16
-
-// let myLinkedList = {
-//   head: {
-//     value: 10,
-//     next: {
-//       value: 5,
-//       next{
-//         value: 16,
-//         next: {
-//           null
-//         }
-//       }
-//     }
-//   }
-// }
-
 class LinkedList {
   constructor(value) {
-    this.head = {
-      value: 10,
-      next: null
-    }
-    this.tail = this.head
-    this.length = 1;
+      this.head = {
+          value: value,
+          next: null
+      };
+      this.tail = this.head;
+      this.length = 1;
   }
   append(value) {
-    //Code here
     const newNode = {
       value: value,
       next: null
     }
-    this.tail.next = newNode
-    this.tail = newNode
-    this.length ++
-    return this
+    console.log(newNode)
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
   }
   prepend(value) {
-    // Code here
-    const firstNode = {
+    const newNode = {
       value: value,
       next: null
     }
-    firstNode.next = this.head
-    this.head = firstNode
-    this.length ++
-    return this
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while(currentNode !== null){
+        array.push(currentNode.value)
+        currentNode = currentNode.next
+    }
+    return array;
+  }
+  insert(index, value){
+    //Check for proper parameters;
+    if(index >= this.length) {
+      console.log('yes')
+      return this.append(value);
+    }
+    
+    const newNode = {
+      value: value,
+      next: null
+    }
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index){
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    // Check Parameters      
+    const leader = this.traverseToIndex(index-1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+  reverse() {
+    // check params
+
+    if (!this.head.next) {
+      return this.head
+    }
+
+    let first = this.head
+    let second = first.next
+
+    while (second) {
+      const temp = second.next
+      second.next = first
+      first - second
+      second = temp
+    }
   }
 }
 
-const myLinkedList = new LinkedList(10)
-
-
-// console.log(myLinkedList);
-
-console.log(myLinkedList.append(5));
-console.log(myLinkedList.append(15));
-console.log(myLinkedList.prepend(99));
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5)
+myLinkedList.append(16)
+myLinkedList.prepend(1)
+myLinkedList.printList()
+myLinkedList.insert(2, 99)
+myLinkedList.insert(20, 88)
+myLinkedList.printList()
+myLinkedList.remove(2)
+myLinkedList.reverse()
